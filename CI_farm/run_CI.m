a5d1519037e761f04430e9dd8a1b132e228c960b
valverdon <60742309+valverdon@users.noreply.github.com>
2020-06-19 17:37:34 +0200

    Ajout commenté de script pour faire tourner plusieurs paramètres à la suite

--- CI_farm/run_CI.m
+++ CI_farm/run_CI.m
@@ -39,10 +39,21 @@ pet = 'Salmo_salar_farm';  % replace my_pet, with your species name
 pets = {'Salmo_salar_farm'};
 mat2pars_init(pet)
 
+% stepCI - Choose the step(s) you want to simulate
+%          0: runs the whole procedure; 
+%          1: runs only the calibration step
+%          2: runs only the profile for a parameter step
+%          3: computes the CI and graphs the profile
+stepCI = 0; 
+
 % parameter for the profile step
 pProfile ='p_M';    % name of the parameter for which the profile will be calculated
-lowVal = 2;   % lower value of the profile interval, depends on the parameter
-upperVal = 5000;  % upper value of the profile interval, depends on the parameter
+%pProfile=["z","v","kap";"p_M","E_Hp"]
+lowVal = 5;   % lower value of the profile interval, depends on the parameter
+%borders taken from addmypet all organisms
+%lowVal=[0.01,0.01,0.01,5,1000]
+upperVal = 7000;  % upper value of the profile interval, depends on the parameter
+%upperVal=[100,0.99,0.99,7000,10000000]
 dim = 20;         % increase for a smooth profile (optional)
 
 % input for the calibration step
@@ -53,13 +64,9 @@ nCont  = 10;       % increase number of continuations, if necessary
 % Specify the confidence level for the confidence interval
 clevel =0.95; % e.g. 0.9 for a 90% confidence interval
 
-% stepCI - Choose the step(s) you want to simulate
-%          0: runs the whole procedure; 
-%          1: runs only the calibration step
-%          2: runs only the profile for a parameter step
-%          3: computes the CI and graphs the profile
 
-stepCI = 0; 
+
+
 tic
 % -------------------------------------------------------------------------
 if stepCI == 0 || stepCI == 1
@@ -74,28 +81,60 @@ end
 toc
 %
 tic
-if stepCI == 0 || stepCI == 2
-    fprintf('\n Step 2. Profile for parameter %s \n\n', pProfile);
-    [lf_profile, pars_profile] = get_profile(pProfile,lowVal,upperVal,dim,nCont);
-    % lf_profile: vector with the values of the loss function
-    % pars_profile: vector with the parameter values 
-
-    save(['profile_',pProfile], 'lf_profile', 'pars_profile')
-end
-toc
+% for parametre in pProfile
+    
+%     if stepCI == 0 || stepCI == 2
+%         fprintf('\n Step 2. Profile for parameter %s \n\n', parametre);
+%         [lf_profile, pars_profile] = get_profile(parametre,lowVal,upperVal,dim,nCont);
+%         % lf_profile: vector with the values of the loss function
+%         % pars_profile: vector with the parameter values 
+% 
+%         save(['profile_',parametre], 'lf_profile', 'pars_profile')
+%     end
+%     toc
+%    
+% %
+%     if  stepCI == 0 || stepCI == 3
+%         fprintf('\n Step 3. Show plots and CI\n\n');
+%         if exist(['profile_',parametre], 'file') == 0 && exist('calibrate.mat', 'file')== 0
+%             disp('Execute steps 1 and 2 first')
+%         else
+%             load(['profile_',parametre])
+%             load('calibrate.mat')
+%             [lf_thres, ci_low, ci_upper] = plot_profile_CI(pars_profile, lf_profile, lf_calibrate, clevel);
+%             xlabel(sprintf('%s',pProfile));
+%        
+%             % confidence interval
+%             fprintf('The CI for %s is from %1.4f to %1.4f \n', parametre, ci_low, ci_upper)
+%             fprintf('The threshold value for the loss function is %1.3f \n', lf_thres)      
+%         end
+%     end
 %
-if  stepCI == 0 || stepCI == 3
-    fprintf('\n Step 3. Show plots and CI\n\n');
-    if exist(['profile_',pProfile], 'file') == 0 && exist('calibrate.mat', 'file')== 0
-        disp('Execute steps 1 and 2 first')
-    else
-        load(['profile_',pProfile])
-        load('calibrate.mat')
-    	[lf_thres, ci_low, ci_upper] = plot_profile_CI(pars_profile, lf_profile, lf_calibrate, clevel);
-         xlabel(sprintf('%s',pProfile));
-       
-        % confidence interval
-        fprintf('The CI for %s is from %1.4f to %1.4f \n', pProfile, ci_low, ci_upper)
-        fprintf('The threshold value for the loss function is %1.3f \n', lf_thres)      
+
+    if stepCI == 0 || stepCI == 2
+        fprintf('\n Step 2. Profile for parameter %s \n\n', pProfile);
+        [lf_profile, pars_profile] = get_profile(pProfile,lowVal,upperVal,dim,nCont);
+        % lf_profile: vector with the values of the loss function
+        % pars_profile: vector with the parameter values 
+
+        save(['profile_',pProfile], 'lf_profile', 'pars_profile')
     end
-end
+    toc
+   
+%
+    if  stepCI == 0 || stepCI == 3
+        fprintf('\n Step 3. Show plots and CI\n\n');
+        if exist(['profile_',pProfile], 'file') == 0 && exist('calibrate.mat', 'file')== 0
+            disp('Execute steps 1 and 2 first')
+        else
+            load(['profile_',pProfile])
+            load('calibrate.mat')
+            [lf_thres, ci_low, ci_upper] = plot_profile_CI(pars_profile, lf_profile, lf_calibrate, clevel);
+            xlabel(sprintf('%s',pProfile));
+       
+            % confidence interval
+            fprintf('The CI for %s is from %1.4f to %1.4f \n', pProfile, ci_low, ci_upper)
+            fprintf('The threshold value for the loss function is %1.3f \n', lf_thres)      
+        end
+    end
+%     end
