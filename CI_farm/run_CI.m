88269a835ee0646c0fd4b97f36af4f2faedf3630
valverdon <60742309+valverdon@users.noreply.github.com>
2020-07-01 16:06:20 +0200

    Results confidance intervals farm

--- CI_farm/run_CI.m
+++ CI_farm/run_CI.m
@@ -40,26 +40,26 @@ global pet pets
 % Choose the step(s) you want to simulate (line58).
 pet = 'Salmo_salar_farm';  % replace my_pet, with your species name
 pets = {'Salmo_salar_farm'};
-mat2pars_init(pet)
+% mat2pars_init(pet)
 
 % stepCI - Choose the step(s) you want to simulate
 %          0: runs the whole procedure; 
-%          1: runs only the calibration step
+%          a1: runs only the calibration step
 %          2: runs only the profile for a parameter step
 %          3: computes the CI and graphs the profile
 %          4: runs only the profile and graphs it (no calibration step)
-stepCI =2; 
+stepCI =3; 
 
 % parameter for the profile step
-pProfile ='v';    % name of the parameter for which the profile will be calculated
+pProfile ={'E_Hp'};    % name of the parameter for which the profile will be calculated
 % pProfile={'z','v','kap','p_M','E_Hp'};
 
 %borders taken from addmypet all organisms
-lowVal = 0.002;   % lower value of the profile interval, depends on the parameter
-% lowVal=[0.4,0.001,0.8,10,1000];
-upperVal = 0.1;  % upper value of the profile interval, depends on the parameter
-% upperVal=[10,0.1,0.9999,15000,1000000];
-dim = 15;         % increase for a smooth profile (optional)
+lowVal = 1000;   % lower value of the profile interval, depends on the parameter
+% lowVal=[0.4,0.025,0.8,10,1000];
+% upperVal = 1000000;  % upper value of the profile interval, depends on the parameter
+upperVal=[10,0.1,0.9999,15000,1000000];
+dim = 20;         % increase for a smooth profile (optional)
 
 % input for the calibration step
 nTrials = 100;   % number of data sets (can be changed)
@@ -103,16 +103,18 @@ for nparametre = 1:size(pProfile,2)
 
     if  stepCI == 0 || stepCI == 3 || stepCI == 4
         fprintf('\n Step 3. Show plots and CI\n\n');
-        if exist(['profile_',pProfile], 'file') == 0 && exist('calibrate.mat', 'file')== 0
+        if exist(['profile_',char(pProfile)], 'file') == 0 && exist('calibrate.mat', 'file')== 0
             disp('Execute steps 1 and 2 first')
         else
-            load(['profile_',pProfile])
+            load(['profile_',char(pProfile)])
             load('calibrate.mat')
             [lf_thres, ci_low, ci_upper] = plot_profile_CI(pars_profile, lf_profile, lf_calibrate, clevel);
-            xlabel(sprintf('%s',pProfile));
-       
+            xlabel(sprintf('%s',char(pProfile)));
+            saveas(gcf,strcat('profil_',sprintf('%s',char(pProfile))),'png');
+            figure(1)
+            saveas(gcf,strcat('survival_',sprintf('%s',char(pProfile))),'png');
             % confidence interval
-            fprintf('The CI for %s is from %1.4f to %1.4f \n', pProfile, ci_low, ci_upper)
+            fprintf('The CI for %s is from %1.4f to %1.4f \n', char(pProfile), ci_low, ci_upper)
             fprintf('The threshold value for the loss function is %1.3f \n', lf_thres)      
         end
     end
